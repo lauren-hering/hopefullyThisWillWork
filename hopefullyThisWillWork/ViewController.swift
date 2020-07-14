42e8026b783aaa78659bb6c2abd19b11d427d562
lauren <lauren.hering@comcast.net>
2020-07-14 14:52:35 -0500

    Initial Commit

---
+++ hopefullyThisWillWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hopefullyThisWillWork
+//
+//  Created by Lauren Hering on 7/14/20.
+//  Copyright © 2020 Lauren Hering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
